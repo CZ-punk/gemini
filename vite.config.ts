3211efb241fadbc29b0decf59a6d6b22b9906452
CZ-punk <czpunk@example.com>
2026-04-25 16:34:38 +0900

    Fix: Revert base path to absolute and aim for Cloudflare fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 배포 시 자산 경로를 상대 경로로 설정
+  base: '/', // 클라우드플레어 배포 시 표준 경로로 설정
 })
